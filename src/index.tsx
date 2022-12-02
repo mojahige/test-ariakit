d576ce9f16098278fbfdce8a33e492e3981ff967
mojahige <mojahige@gmail.com>
2022-12-03 00:02:07 +0900

    Fix "no-non-null-assertion"

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement!);
+
+if (!rootElement) {
+  throw new Error(
+    "The app could not execute createRoot because the element to mount did not exist."
+  );
+}
+
+const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
